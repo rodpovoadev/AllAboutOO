66a2162ddcde07220847e1fbdd7151facda32e5a
Rodolfo Povoa Leal <netfornes@hotmail.com>
2016-06-27 19:35:39 -0300

    #2 - Criação do Layout e Classes com implementação de Classes / Métodos / Herança  e Sobrecarga de Métodos

--- AllAboutOO/Class/HerançaMetodosConstrutoresSobrecarga.cs
+++ AllAboutOO/Class/HerançaMetodosConstrutoresSobrecarga.cs
@@ -1,50 +1,58 @@
-﻿using AllAboutOO.Controllers;
+﻿/*Autor: Rodolfo Póvoa Leal
+  
+
+ */
+
+using AllAboutOO.Controllers;
 
 namespace AllAboutOO.Class
 {
 
-        public class Aluno : Person
+    public class Aluno : Person
+
+    {
+
+        //Método Construtor da Classe Aluno
+        public Aluno()
 
         {
+            //Aqui fica o código a ser implementado no método Construtor
+        }
 
-        
-            public Aluno()
+        //Método destrutor Classe Aluno
+        ~Aluno()
+        {
+            //Aqui fica o código do método destrutor
+        }
 
-            {
-                //Aqui fica o código a ser implementado no método Construtor
-            }
+        //Atributos especificos da Classe Pessoa. Os outros atributos Nome , Sobrenome, CPF, DataDeNascimento, são herdados da classe Person
+        public long Matricula { get; set; }
+        public string DataDeCadastro { get; set; }
 
-            ~Aluno()
-            {
-                //Aqui fica o código do método destrutor
-            }
+        //Método para criação de um novo aluno
+        public virtual Aluno NovoAluno()
+        {
 
-          public int Matricula { get; set; }
-          public string DataDeCadastro { get; set; }
+            Aluno aluno = new Aluno();
 
-        
-          public Aluno NovoAluno()
-             {
+            aluno.CPF = "100.677.243-55";
+            aluno.Nome = "Anderson";
+            aluno.Sobrenome = "Silva";
+            aluno.DataDeNascimento = "13/01/1990";
+            aluno.TelefoneCelular = "21 98832-6699";
+            aluno.Rua = "Av Nossa Senhora de Copacabana";
+            aluno.Bairro = "Copacabana";
+            aluno.Numero = 79;
+            aluno.Matricula = 25452;
+            aluno.DataDeCadastro = "15/10/2005";
 
-                  Aluno aluno = new Aluno();
+            return aluno;
 
-                  aluno.CPF = "100.677.243-55";
-                  aluno.Nome = "Anderson";
-                  aluno.Sobrenome = "Silva";
-                  aluno.DataDeNascimento = "13/01/1990";
-                  aluno.TelefoneCelular = "21 98832-6699";
-                  aluno.Rua = "Av Nossa Senhora de Copacabana";
-                  aluno.Bairro = "Copacabana";
-                  aluno.Numero = 79;
-                  aluno.Matricula = 25452;
-                  aluno.DataDeCadastro = "15/10/2005";
+        }
 
-                  return aluno;
-
-                }
 
         /* Na classe aluno estamos fazendo uma Herança, herdando atributos e métodos da classse Person. Vamos pensar no seguinte, um aluno tem os mesmos atributos da 
-           classe Person, um Aluno tem nome, sobrenome, data de nascimento, Rua, Bairro, Telefone Celuler e etc. Para não precisar reencrever esses atributos em outra classe
+           classe Person, um Aluno tem nome, sobrenome, data de nascimento, Rua, Bairro, Telefone Celuler e etc. Para não precisar reescrever esses atributos em outra classe
            implementaremos a herança. A interface é implementada da seguinte forma, SuaClasse : ClasseQueQueroHerdar.
 
            Note que no método NovoAluno(), estamos preenchendo atributos que estão na classe Person e foram herdados para a classe Aluno. Os atributos Matricula e Data de Cadastro
@@ -65,9 +73,42 @@ namespace AllAboutOO.Class
            O método Aluno() é um método construtor e será executado na criação do objeto, ou seja, assim que a classe é instanciada.
        */
 
+        //Método para validar um campo
+        public bool ValidField(long matricula)
+        {
+            if (matricula == 0)
+            {
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
 
+        //Sobrecarga do método acima
+        public bool ValidField(string CPF)
+        {
 
+            if (CPF.Length == 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
+        /*O método  - public bool ValidField(long matricula) - está sendo sobrecarregado. Existe um outro método ( - public bool ValidField(string CPF) - ) com mesmo nome porem com o tipo de parâmetro
+          diferente, no primeiro o método recebe um parâmetro do tipo Long  e no segundo o método recebe um parâmetro do tipo string. Na hora de chamar o método o tipo de dado que eu passar pra ele,
+          define qual método será chamado.
+
+          A sobrecarga de métodos é utilizada quando queremos criar um vários métodos com funções parecidas, ou até iguais, mas que tenham diferentes tipos de dados.
+         */
     }
+
+
+
 
 }
